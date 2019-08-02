ec5553e23f332f1a2d17d6c0162712880d02b7fd
Hidan <pataricza1987@gmail.com>
2019-08-02 23:34:33 +0200

    application.properties and basic OAuth2Config added

---
+++ src/main/java/hello/OAuth2Config.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class OAuth2Config extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+      .antMatchers("/", "/home").permitAll()
+      .anyRequest().authenticated();
+  }
+}
